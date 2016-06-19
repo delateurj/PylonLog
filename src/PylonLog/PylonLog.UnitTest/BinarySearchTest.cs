dcce5d8c2ec941e0d9dff455f096f854c5922984
delateurj <joe.delateur@gmail.com>
2016-06-19 14:29:38 -0700

    Initial Commit

---
+++ src/PylonLog/PylonLog.UnitTest/BinarySearchTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PylonLog.Utilities;
+
+namespace PylonLog.UnitTest
+{
+    [TestClass]
+    public class BinarySearchTest
+    {
+        [TestMethod]
+        public void Test_Pattern_Longer_Than_Target()
+        {
+            byte[] targetArray = { 12 };
+            byte[] patternArray = { 12, 12 };
+
+            Assert.AreEqual(-1, ( BinarySearch.findPattern(targetArray, patternArray)));
+        }
+
+        [TestMethod]
+        public void No_Match()
+        {
+            byte[] targetArray = { 12 };
+            byte[] patternArray = { 11 };
+
+            Assert.AreEqual(-1, (BinarySearch.findPattern(targetArray, patternArray)));
+        }
+
+        [TestMethod]
+        public void Empty_Pattern()
+        {
+            byte[] targetArray = { 12 };
+            byte[] patternArray = {  };
+
+            Assert.AreEqual(-1, (BinarySearch.findPattern(targetArray, patternArray)));
+        }
+
+        [TestMethod]
+        public void Empty_Target()
+        {
+            byte[] targetArray = {  };
+            byte[] patternArray = { 12};
+
+            Assert.AreEqual(-1, (BinarySearch.findPattern(targetArray, patternArray)));
+        }
+
+        [TestMethod]
+        public void Test_Pattern_Partial_Match()
+        {
+            byte[] targetArray = { 1, 2, 3, 4 };
+            byte[] patternArray = { 4, 12 };
+
+            Assert.AreEqual(-1, (BinarySearch.findPattern(targetArray, patternArray)));
+        }
+
+        [TestMethod]
+        public void Test_Pattern_One_Char_Match()
+        {
+            byte[] targetArray = { 12 };
+            byte[] patternArray = { 12 };
+
+            Assert.AreEqual(0, (BinarySearch.findPattern(targetArray, patternArray)));
+        }
+
+        [TestMethod]
+        public void Test_Pattern_One_Char_Match_At_End()
+        {
+            byte[] targetArray = { 1,2,3,4,12 };
+            byte[] patternArray = { 12 };
+
+            Assert.AreEqual(4, (BinarySearch.findPattern(targetArray, patternArray)));
+        }
+
+        [TestMethod]
+        public void Test_Pattern_Two_Char_Match_At_End()
+        {
+            byte[] targetArray = { 1, 2, 3, 4, 12 };
+            byte[] patternArray = { 4,12 };
+
+            Assert.AreEqual(3, (BinarySearch.findPattern(targetArray, patternArray)));
+        }
+    }
+}
---
+++ src/PylonLog/PylonLog.Utilities/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PylonLog.Utilities
+{
+    public static class BinarySearch
+    {
+        public static int findPattern(byte[] target, byte[] pattern)
+        {
+            if (pattern.Length == 0 || target.Length == 0)
+            {
+                return -1;
+            }
+            var len = pattern.Length;
+            var limit = target.Length - len;
+            for (var i = 0; i <= limit; i++)
+            {
+                var k = 0;
+                for (; k < len; k++)
+                {
+                    if (pattern[k] != target[i + k]) break;
+                }
+                if (k == len) return i;
+            }
+            return -1;
+        }
+    }
+}
