d81e75ad49e3c681b6150a9609f0641036f837cd
delateurj <joe.delateur@gmail.com>
2016-06-24 14:31:50 -0700

    First commit

---
+++ src/PylonLog/PylonLog.Utilities/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PylonLog.Utilities
+{
+    public static class Constants
+    {
+        public static readonly byte[] NEW_SESSION_LOG_PATTERN = { 0xff, 0xff, 0xff, 0xff };
+
+        public const int HEADER_BLOCK_LENGTH = 36;
+
+        public static readonly byte[] LAST_SUPPLEMENTAL_HEADER_PATTERN = { 0x17, 0x17 };
+
+        public const int DATA_BLOCK_LENGTH = 20;
+
+        public const int PLANE_NAME_OFFSET = 12;
+    }
+}
