23c736902c508c2761d0dbba685a8da01b1509b4
delateurj <joe.delateur@gmail.com>
2016-08-28 10:17:16 -0700

    Starting to add window for maintaining related data like plugs, props.  Also pushing a log file that had the data that necessitate change in previous commit.

---
+++ src/PylonLog/PylonLog.Core/wndMaintainRelatedData.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PylonLog.Core
+{
+    /// <summary>
+    /// Interaction logic for wndMaintainRelatedData.xaml
+    /// </summary>
+    public partial class wndMaintainRelatedData : Window
+    {
+        public wndMaintainRelatedData()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSavePlugs_Click(object sender, RoutedEventArgs e)
+        {
+            GlobalDataContext.pylonLogContext.SaveChanges();
+        }
+    }
+}
